ef0d694de87a7db9cfe9c8e03b6182a853a9976c
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-01-15 17:59:56 +0900

    Commit Before Dinner

---
+++ Assets/Scripts/moveBlock.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBlock : MonoBehaviour
+{
+    public float dir = 1f;
+    public float speed = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, speed * dir));
+    }
+}
